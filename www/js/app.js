346a0d29e5407e7c7e130970bd67a8baff33fc97
Lucas <lucashen@gmail.com>
2015-07-11 10:57:35 -0300

    multi language app example

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'pascalprecht.translate'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,27 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, $translateProvider) {
+    $translateProvider.translations('en', {
+        TITLE: "Hello",
+        TEXT_NAME: "Enter your name"
+    });
+    $translateProvider.translations('es', {
+        TITLE: "Hola",
+        TEXT_NAME: "Introduzca su nombre"
+    });
+    $translateProvider.translations('pt', {
+        TITLE: "Olá",
+        TEXT_NAME: "Digite o seu nome"
+    });
+    $translateProvider.preferredLanguage("en");
+})
+
+.controller('MainCtrl', function($scope, $translate) {
+
+    $scope.ChangeLanguage = function(lang){
+        $translate.use(lang);
+    }
+
+});
